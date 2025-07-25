61775bbf66d2939f2354a1c27ff35d326d771274
muhammed <masalim122006@gmail.com>
2025-07-25 09:02:48 +0300

    some repairs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
